495fd082c81c2288edb285ea8589ac97b369016c
Владислав Шевела <shevela@algont.ru>
2020-05-19 11:15:18 +0300

    Added header slider and menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-
+    $('.slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
+        dots: true,
+    })
+    $(window).on('scroll', function () {
+        if($(window).scrollTop()){
+            $('nav').addClass('bg-menu-scroll');
+        } else {
+            $('nav').removeClass('bg-menu-scroll');
+        }
+    })
 });
